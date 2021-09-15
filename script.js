b236926e2ae1442bad9d57db3448df0286f84872
TSilchenko <taniasilchenko212@gmail.com>
2021-09-15 15:41:55 +0300

    feat: tasks array

--- script.js
+++ script.js
@@ -38,4 +38,73 @@ this.addMethod = function (name, func) {
 
 let calc = new Calculator;
 
-console.log(calc.calculate("2 + 3"))
+// console.log(calc.calculate("2 + 3"))
+
+
+// let vasya = { name: "Вася", age: 25 };
+// let petya = { name: "Петя", age: 30 };
+// let masha = { name: "Маша", age: 28 };
+
+// let users = [ vasya, petya, masha ];
+
+// let names = users.map(el => el.name)
+
+// console.log(names)
+
+
+// let vasya = { name: "Вася", surname: "Пупкин", id: 1 };
+// let petya = { name: "Петя", surname: "Иванов", id: 2 };
+// let masha = { name: "Маша", surname: "Петрова", id: 3 };
+
+// let users = [ vasya, petya, masha ];
+
+// let usersMapped = users.map((el,i) =>{
+//     return {fullName: el.name + " " + el.surname, id: i+1}
+// })
+
+// console.log(usersMapped[0].id)
+// console.log(usersMapped[0].fullName)
+
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 29 };
+
+let arr = [ vasya, petya, masha ];
+
+function sortByAge(arr) {
+return arr.sort((a,b)=> a.age - b.age)
+}
+
+// console.log(sortByAge(arr));
+
+// let arr = [1, 2, 3];
+
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+// console.log(shuffle(arr))
+// console.log(shuffle(arr))
+// console.log(shuffle(arr))
+
+
+function getAverageAge(arr){
+    return arr.reduce((sum, cur) => sum +=cur.age,0)/arr.length
+}
+
+
+// console.log(getAverageAge(arr))
+
+let strings = ["кришна", "кришна", "харе", "харе",
+"харе", "харе", "кришна", "кришна", ":-O"
+];
+
+function unique(arr) {
+   return new Set(arr)
+}
+  
+console.log(unique(strings))
